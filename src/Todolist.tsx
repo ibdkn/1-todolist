cc22b0151252988248007094ae9ee803dbe2f4f5
ibdkn <ibudiakina@gmail.com>
2024-03-25 16:36:45 +0300

    add map

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -20,9 +20,14 @@ export const Todolist = ({ title, tasks }: PropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" defaultChecked={tasks[0].isDone}/> <span>{tasks[0].title}</span></li>
-                <li><input type="checkbox" defaultChecked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>
-                <li><input type="checkbox" defaultChecked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>
+                {tasks.map(task => {
+                    return (
+                        <li>
+                            <input type="checkbox" checked={task.isDone} />
+                            <span>{task.title}</span>
+                        </li>
+                    )
+                })}
             </ul>
             <div>
                 <button>All</button>
